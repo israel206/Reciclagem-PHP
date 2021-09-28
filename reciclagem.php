1977596cfc9fdaa7b871e9c3589e4849c0684019
israel <israel.pereira@dcx.ufpb.br>
2021-09-28 13:06:11 -0300

    variavel📗

--- reciclagem.php
+++ reciclagem.php
@@ -9,13 +9,50 @@
 </head>
 
 <body>
+
   <?php
   echo "Revisando contextos do php, para fica atualizados na linguagem";
   $nome = "Israel Silva";
   $idade = 33;
   $altura = 1.71;
+  echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+  echo "<hr>";
+
+  // Alterando meus dados
+
+  echo "Revisando contextos do php, para fica atualizados na linguagem <br>";
+  $nome = "Israel Pereira";
+  $idade = 32;
+  $altura = 1.70;
   echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura ";
+  echo "<hr>";
+
+  $carro = "Jeep";
+  $Carro = "Hilux";
+  echo $carro;
+  echo "<hr>";
+  echo $Carro;
+
+  $meunome = "Israel";
+  $meuNome = "Charlles";
+  echo $meunome;
+  echo "<hr>";
+  echo $meuNome;
+
+  // Nomes válidos
+  // $nome
+  // $_meunome
+  // $carro01
+  // $meu_nome
+  // $meuNome
+
+  //Nomes Inválidos
+  // $123nome
+  // $carro!#@%
+  // $meu carro
+
   ?>
+
 </body>
 
 </html>
