04af6170d8aa8757238ca0b0b8b6cc66360b0bbb
israel <israel.pereira@dcx.ufpb.br>
2021-09-28 12:45:59 -0300

    revisando conceitos📚

---
+++ reciclagem.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Revisando Contextos do PHP</title>
+</head>
+
+<body>
+  <?php
+  echo "Revisando contextos do php, para fica atualizados na linguagem";
+  $nome = "Israel Silva";
+  $idade = 33;
+  $altura = 1.71;
+  echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura ";
+  ?>
+</body>
+
+</html>
