043d6955b0b69cd525dafc0f7343831143dfae64
israel <israel.pereira@dcx.ufpb.br>
2021-11-07 12:43:56 -0300

    add file de home

---
+++ cadastro-php/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Sistema</title>
+
+  <link rel="stylesheet" href="css/home.css">
+
+</head>
+
+<body>
+
+  <h1>Sistema de apresentação ;)</h1>
+  <h2>Developer israel</h2>
+
+  <div class="box">
+    <a href="login.php">Login</a>
+    <a href="formulario.php">Cadastre-se</a>
+  </div>
+
+</body>
+
+</html>
